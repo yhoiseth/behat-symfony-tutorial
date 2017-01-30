e4d2cadee75fd7cc792d1f135b59ebf7c38f746c
Yngve Høiseth <yhoiseth@gmail.com>
2017-01-30 18:40:49 +0100

    Run Behat using Chrome

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -7,7 +7,7 @@ use Behat\Gherkin\Node\TableNode;
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext implements Context
+class FeatureContext extends \Behat\MinkExtension\Context\MinkContext implements Context
 {
     /**
      * Initializes context.
---
+++ features/javascript.feature
@@ -0,0 +1,6 @@
+Feature: JavaScript
+
+  Scenario: Execute JS
+    Given I am on "/"
+    Then I should see "Symfony 2.8.16"
+    Then I should see "Added using JS"
